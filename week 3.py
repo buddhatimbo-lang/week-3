8f13d8c372a2a4a60ef3c24311b55583ef2cf20c
buddhatimbo-lang <buddhatimbo@gmail.com>
2026-04-26 13:54:37 +0100

    Add files via upload

---
+++ week 3.py
@@ -0,0 +1,9 @@
+first_name = input("enter first_name : ")
+last_name = input("enter last_name : ")
+age = input("enter age : ")
+
+# concate
+full_name = first_name + " " + last_name
+# print result
+print ("Full_name:", full_name)
+print ("Age:",age)
